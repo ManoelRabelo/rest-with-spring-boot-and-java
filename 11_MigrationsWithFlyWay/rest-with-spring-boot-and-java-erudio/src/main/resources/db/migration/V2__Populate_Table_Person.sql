282a922894289fb898e692c26d1db567795a9291
Manoel <manoelrabelo7@gmail.com>
2024-01-03 21:43:13 -0300

    11_MigrationsWithFlyWay

---
+++ 11_MigrationsWithFlyWay/rest-with-spring-boot-and-java-erudio/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,8 @@
+INSERT INTO `person` (`id`,`address`,`first_name`,`gender`,`last_name`) VALUES 
+	(1,'Samambaia','Manoel','male','Rabelo'),
+	(2,'Taguatinga','Vitoria','female','Sousa'),
+	(4,'Samambaia','Manoel','male','Ribas'),
+	(5,'Samambaia','Zelandia','female','Oliveira'),
+	(7,'logo ali','Fulano','male','mais outro'),
+	(9,'Samambaia','Manoel','male','Rabelo'),
+	(10,'Texas','Sheldon','male','Copper');
