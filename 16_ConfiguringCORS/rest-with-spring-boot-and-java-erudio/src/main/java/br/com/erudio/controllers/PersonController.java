fca6377751bd3c5c055e883b9b0df2e1965b8646
Manoel <manoelrabelo7@gmail.com>
2024-02-07 22:35:36 -0300

    16_ConfiguringCORS

--- 16_ConfiguringCORS/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/controllers/PersonController.java
+++ 16_ConfiguringCORS/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/controllers/PersonController.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,7 +23,6 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
-//@CrossOrigin
 @RestController
 @RequestMapping("/api/person/v1")
 @Tag(name = "People", description = "Endpoints for Managing People")
@@ -54,7 +52,6 @@ public class PersonController {
 		return service.findAll();
 	}
 
-	@CrossOrigin(origins = "http://localhost:8080")
 	@GetMapping(
 			value = "/{id}", 
 			produces = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML })
@@ -74,7 +71,6 @@ public class PersonController {
 		return service.findById(id);
 	}
 
-	@CrossOrigin(origins = {"http://localhost:8080", "https://erudio.com.br"})
 	@PostMapping(
 			consumes = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML }, 
 			produces = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML })
---
+++ 16_ConfiguringCORS/rest-with-spring-boot-and-java-erudio/src/main/resources/db/migration/V4__Insert_Data_in_Books.sql
@@ -0,0 +1,4 @@
+INSERT INTO `books` VALUES
+	(1, 'Manoel', '2024-01-10 00:00:00.000000', 22.99,'Livro exemplo'),
+	(2, 'Vitoria', '2023-01-01 00:00:00.000000', 30.00, 'Livro de ler'),
+	(3, 'Maira', '2010-10-10 00:00:00.000000', 14.99, 'Oto Livro');
