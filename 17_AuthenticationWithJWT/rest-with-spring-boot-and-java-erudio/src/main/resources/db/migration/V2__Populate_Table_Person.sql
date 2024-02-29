93d24f1c338427cecbd1b4f8f1c29c7130d0cc01
Manoel <manoelrabelo7@gmail.com>
2024-02-28 21:58:31 -0300

    17_AuthenticationWithJWT

---
+++ 17_AuthenticationWithJWT/rest-with-spring-boot-and-java-erudio/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,10 @@
+INSERT INTO `person` (`id`,`address`,`first_name`,`gender`,`last_name`) VALUES 
+	(1,'Samambaia','Manoel','male','Rabelo'),
+	(2,'Taguatinga','Vitoria','female','Sousa'),
+	(4,'Samambaia','Manoel','male','Ribas'),
+	(5,'Samambaia','Zelandia','female','Oliveira'),
+	(7,'logo ali','Fulano','male','mais outro'),
+	(9,'Samambaia','Manoel','male','Rabelo'),
+	(10,'Texas','Sheldon','male','Copper'),
+	(11,'Maira','Samantha', 'female', 'Guará II'),
+	(12,'Pedro','Rabelo', 'male', 'Porto seguro-BA');
