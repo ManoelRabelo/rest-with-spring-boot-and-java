f152f45f3878e58d5600b5752cd30bcfc695f580
Manoel <manoelrabelo7@gmail.com>
2024-06-06 09:07:43 -0300

    22_DockerizingOurApp

---
+++ 22_DockerizingOurApp/rest-with-spring-boot-and-java-erudio/src/main/resources/db/migration/V4__Insert_Data_in_Books.sql
@@ -0,0 +1,9 @@
+INSERT INTO `books` VALUES
+	(1, 'Eiichiro Oda', '1977-07-22 00:00:00.000000', 19.99,'One piece'),
+	(2, 'Akira Toriyama', '1984-11-20 00:00:00.000000', 9.99, 'Dragon Ball'),
+	(3, 'Akira Toriyama', '1989-04-26 00:00:00.000000', 15.00, 'Dragon Ball Z'),
+	(4, 'Masashi Kishimoto', '1999-09-21 00:00:00.000000', 24.99, 'Naruto'),
+	(5, 'Yoshihiro Togashi', '1998-03-03 00:00:00.000000', 30.00, 'Hunter x Hunter'),
+	(6, 'R. F. Kuang', '2018-05-01 00:00:00.000000', 47.99, 'A guerra da papoula'),
+	(7, 'Xiran Jay Zhao', '2021-09-21 00:00:00.000000', 27.88, 'Viúva de Ferro'),
+	(8, 'Chuck Palahniuk', '1996-08-01 00:00:00.000000', 47.99, 'Clube da luta');
